ced6a0d79a186eef70d4d8de79d97c390f8f49ff
Huda1020 <70962141+Huda1020@users.noreply.github.com>
2020-09-29 15:10:22 -0400

    Add files via upload

---
+++ EmailAddress.java
@@ -0,0 +1,26 @@
+/**
+ *Author: Huda Abdulle
+ *Date: Sep 24, 2020
+ *Description:program assigns a new shark mail address to students (sharkmail.com)
+ *system
+ */
+import java.util.Scanner;
+public class EmailAddress {
+	public static void main(String[] args) {
+	Scanner keyboard = new Scanner(System.in);	
+	System.out.println(" Welcome to sharkmail registration, what is your first name?");
+	String firstName = keyboard.nextLine();
+	System.out.println("What is your middle name?");
+	String middleName = keyboard.nextLine();
+	System.out.println("What is your last name?");
+	String lastName = keyboard.nextLine();
+	System.out.println("What is your birthdate?(mmddyyyy) ");
+	String birthDate = keyboard.nextLine();
+	System.out.println("Email:" + lastName.toLowerCase() + firstName.substring(0,1)     + middleName.substring(0,1)+  "@sharkmail.com" );
+	System.out.println("Temp Password:"  + birthDate );
+	System.out.println("*** This temporary password is very insecure. Please change it immediately!***");
+   //String lastName = keyboard.nextLine();
+	
+
+}
+}
